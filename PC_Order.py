b9a2e21bf4195bea64bb3a664ed0aca33906f756
Administrator <yueyizx@163.com>
2016-06-20 20:41:32 +0800

    使用了线程池，可以指定运行线程的总数量
    关闭主窗口，导致图形界面无响应

--- PC_Order.py
+++ PC_Order.py
@@ -1,11 +1,13 @@
 # coding: utf-8
 import inspect
 import logging
+import os
 import sys
 import time
 import subprocess
+import json
+import codecs
 
-from threading import Thread
 from PySide.QtCore import *
 from PySide.QtGui import *
 from selenium import webdriver
@@ -26,8 +28,9 @@ logging.getLogger('').addHandler(console)
 
 reload(sys)
 sys.setdefaultencoding('utf8')
+module_ctx = dict()
 thread_list = list()
-
+thread_pools = list()
 url = "https://www.amazon.cn/ap/signin?_encoding=UTF8&openid.assoc_handle=cnflex&openid.claimed_id=\
 http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=\
 http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=\
@@ -40,6 +43,9 @@ dcap["phantomjs.page.settings.userAgent"] = (
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/53 "
     "(KHTML, like Gecko) firefox/15.0.87"
 )
+default_thread_num = 1
+current_f_list = os.listdir(os.path.dirname(os.path.abspath('__file__')))
+loop_count = 0
 
 
 def lineno():
@@ -49,11 +55,13 @@ def lineno():
 
 def csv_processing(filename):
     with open(filename) as f:
-        base_info = f.read()
-
+        base_info = f.read().decode('gb18030`').encode('utf-8')
     pre_user_info_list = [i.split(',') for i in base_info.split('\n') if i]
-
-    return pre_user_info_list
+    user_info_list = list()
+    for u in pre_user_info_list:
+        [u.insert(1, '') for r in range(2)]
+        user_info_list.append(u)
+    return user_info_list
 
 
 class OrderSg(QObject):
@@ -63,41 +71,51 @@ class OrderSg(QObject):
         QObject.__init__(self)
 
 
-class OrderThread(QThread):
+class OrderThread(QRunnable):
     """
     This for Order Thread
     """
 
-    def __init__(self, row_index, user_row, tbrowser, ordths):
+    def __init__(self, row_index, user_row, tbrowser):
+        """
+        """
         super(OrderThread, self).__init__()
+        self.emitter = QObject()
         self.sg = OrderSg()
         self.name = 'i_order_thread_{}'.format(row_index)
         self.exiting = True
         self.row_index = row_index
         self.user_row = user_row
         self.tbrowser = tbrowser
-        self.orderthreads = ordths
 
     def run(self):
-        print("Starting{},{}".format(self.name, self.isRunning()))
-        ctx = {'row_index': self.row_index, 'tbrowser': self.tbrowser, 'user_row': self.user_row}
+        """
+        """
+        print("Starting{}".format(self.name))
+        ctx = {'row_index': self.row_index, 'tbrowser': self.tbrowser,
+               'user_row': self.user_row, 'current_thread': self}
         if not self.exiting:
             mo = MakeOrder(ctx)
             mo.make_order()
 
 
 class MakeOrder(object):
+    """
+    """
+
     def __init__(self, data):
         """
         It Support for to use two kind of browsers which is phantomjs and firefox respectively.
         :param data:
         """
-        self.driver = webdriver.PhantomJS(
-            executable_path=phantomjs_path, desired_capabilities=dcap)
-        # self.driver = webdriver.Firefox()
+        # self.driver = webdriver.PhantomJS(
+        #     executable_path=phantomjs_path, desired_capabilities=dcap)
+        self.driver = webdriver.Firefox()
         self.data = data
 
     def check_exists_by_id(self, ele_id):
+        """
+        """
         try:
             self.driver.find_element_by_id(ele_id)
         except NoSuchElementException:
@@ -105,11 +123,15 @@ class MakeOrder(object):
         return True
 
     def make_order(self):
+        """
+        """
         i = self.data['row_index']
         t = self.data['tbrowser']
         u = self.data['user_row']
+        print(lineno(), u)
         username = u[0]
-        password = u[1]
+        password = u[3]
+        print(username, password)
         self.driver.set_window_size(1366, 768)
         self.driver.get(url)
         self.driver.save_screenshot('login_screen.png')
@@ -129,7 +151,7 @@ class MakeOrder(object):
         if self.check_exists_by_id("auth-error-message-box"):
             t.printlog(i, '密码不正确')
         t.printlog(i, '登录成功')
-        print(u'当前用户的cookies字典内容：{}'.format(self.driver.get_cookies()))
+        # print(u'当前用户的cookies字典内容：{}'.format(self.driver.get_cookies()))
         self.driver.save_screenshot('login_success.png')
         time.sleep(1)
         self.driver.get(
@@ -137,8 +159,9 @@ class MakeOrder(object):
             ref_=pu_redeem_coupon&encryptedPromotionId=A28K8TGDBC7AKO")
         time.sleep(1)
         self.driver.delete_all_cookies()
-        print(u'已经删除了cookies： {}'.format(self.driver.get_cookies()))
+        # print(u'已经删除了cookies： {}'.format(self.driver.get_cookies()))
         self.driver.close()
+        t.unchecked(i)
 
 
 class TextBrowser(QWidget):
@@ -147,54 +170,66 @@ class TextBrowser(QWidget):
     """
 
     def __init__(self):
+        """
+        """
         super(TextBrowser, self).__init__()
-        table_head = [u'编号', u'选中', u'登录名', u'运行日志', u'密码',
-                      u'商品编号', u'购买数量', u'省', u'市', u'县/区', u'详细地址',
-                      u'邮编', u'收货人', u'手机号码',
-                      ]
+        self.table_head = [u'登录名', u'选中', u'运行日志', u'密码',
+                           u'商品编号', u'购买数量', u'省', u'市', u'县/区', u'详细地址',
+                           u'邮编', u'收货人', u'手机号码',
+                           ]
         hbox = QHBoxLayout(self)
         self.textbr = QTableWidget()
         hbox.addWidget(self.textbr)
         font = QFont("Courier New", 14)
         self.textbr.setFont(font)
         self.textbr.resizeColumnsToContents()
-        self.textbr.setColumnCount(14)
-        for i, h in enumerate(table_head):
-            self.idx = QTableWidgetItem(h)
-            self.textbr.setHorizontalHeaderItem(i, self.idx)
-        self.newItem = QTableWidgetItem(u"中文测试")
-
-        self.textbr.setItem(1, 1, self.newItem)
         self.textbr.setColumnWidth(3, 200)
         self.textbr.setColumnWidth(1, 200)
         # self.textbr.setEditTriggers(QAbstractItemView.NoEditTriggers)
         self.setLayout(hbox)
 
     def compose_table(self, data):
+        """
+        Construct our table to show a data in the csv file.
+        :parameter data: a dict.
+        """
         user_rows = data['user_rows']
+        print(lineno(), 'user_row_is: {}'.format(len(user_rows)))
         row_count = len(user_rows)
         self.textbr.setRowCount(row_count)
+        self.textbr.setColumnCount(len(self.table_head))
+        self.textbr.setHorizontalHeaderLabels(self.table_head)
+        print(lineno(), 'table column is {}'.format(len(self.table_head)))
         for i, u in enumerate(user_rows):
-            self.textbr.setVerticalHeaderItem(i, QTableWidgetItem(u'   '))
-            self.textbr.setItem(i, 0, QTableWidgetItem(unicode(i + 1)))
-            self.textbr.setItem(i, 1, QTableWidgetItem(
-                unicode(u[0])))
-            self.textbr.setItem(
-                i, 4, QTableWidgetItem(unicode(u[1])))
-            chkitem = QTableWidgetItem()
-            setattr(self, 'table_item_{}'.format(i), chkitem)
-            chkitem.setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
-            chkitem.setCheckState(Qt.Checked)
-            self.textbr.setItem(i, 2, chkitem)
-            chkitem.setSelected(True)
+            self.textbr.setRowHeight(i, 35)
+            print(lineno(), 'per user row coloumn length {}'.format(len(u)))
+            for indx in range(len(self.table_head)):
+                item = QTableWidgetItem(unicode(u[indx]))
+                setattr(self, 'table_item_{}_{}'.format(i, indx), item)
+                if indx == 1:
+                    item.setCheckState(Qt.Checked)
+                    self.textbr.setItem(i, 1, item)
+                else:
+                    self.textbr.setItem(i, indx, item)
 
     def printlog(self, i, s):
-        self.textbr.setItem(i, 3, QTableWidgetItem(unicode(s)))
+        """
+        This used to print log in function make_order.
+        """
+        self.textbr.setItem(i, 2, QTableWidgetItem(unicode(s)))
+
+    def unchecked(self, i):
+        """
+        To uncheck row in the table.
+        """
+        item = QTableWidgetItem()
+        item.setCheckState(Qt.Unchecked)
+        self.textbr.setItem(i, 1, item)
 
 
 class MajorTabWindow(QWidget):
     """
-    add three tab widget to main tab window
+    Add three tab widget to main tab window
     """
 
     def __init__(self, parent=None):
@@ -216,14 +251,20 @@ class MajorTabWindow(QWidget):
         self.setGeometry(600, 600, 700, 600)
 
     def closeEvent(self, event):
-
+        """
+        Show the dialog when user try to close main window.
+        """
         reply = QMessageBox.question(self, 'Message',
                                      u"确定关闭窗口?", QMessageBox.Yes |
                                      QMessageBox.No, QMessageBox.No)
-
         if reply == QMessageBox.Yes:
-            subprocess.call(
-                ['ps -ef | grep phantomjs | grep -v grep | cut -c 6-15'])
+            try:
+                subprocess.call(
+                    ['ps -ef | grep phantomjs | grep -v grep | cut -c 6-15'])
+            except Exception as e:
+                pass
+            if QThreadPool.globalInstance().activeThreadCount():
+                QThreadPool.globalInstance().waitForDone()
             event.accept()
         else:
             event.ignore()
@@ -248,6 +289,10 @@ class Captcha(QWidget):
 
 
 class IndexTabButton(QWidget):
+    """
+    To show three buttons in first tabwidget.
+    """
+
     def __init__(self):
         super(IndexTabButton, self).__init__()
         self.filebutton = QPushButton(u'打开文件')
@@ -265,43 +310,79 @@ class IndexTabButton(QWidget):
 
     def openfiledialog(self):
         """
-        transfer
+        Transfer file's name and the user rows that used be in table.
         """
         dialog = QFileDialog()
         fname, _ = dialog.getOpenFileName(self, 'Open file', '/home')
         user_rows = csv_processing(fname)
         setattr(self, 'filname', fname)
         setattr(self, 'user_rows', user_rows)
-        self.tbrowser.compose_table({'filename': fname, 'user_rows': user_rows})
+        self.tbrowser.compose_table(
+            {'filename': fname, 'user_rows': user_rows})
+        print(lineno(), user_rows)
+
+    @staticmethod
+    def get_thread_num():
+        """
+        Fetch  user config thread number.
+        """
+        if 'user_config.json' in current_f_list:
+            with open('user_config.json') as raw_f:
+                f = json.load(raw_f)
+                user_thread_num = f.get('user_thread_num')
+                return user_thread_num
+        else:
+            return default_thread_num
+
+    def get_checked_rows(self, u_rows):
+        """
+        Through for loop to check a row in the table whether be checked or not.
+        """
+        checked_rows = list()
+        for i, u in enumerate(u_rows):
+            ti = getattr(self.tbrowser, 'table_item_{}_1'.format(i))
+            if ti.checkState() == Qt.Checked:
+                checked_rows.append((i, u))
+        print(lineno(), len(checked_rows))
+        return checked_rows
 
     def startorder(self):
         """
         Before start orderthread we should to check chkitem's checked state.
-        :return:
+        :return: nothing
         """
-        orderthreads = list()
         user_rows = self.user_rows
-        checked_rows = list()
-        for i, u in enumerate(user_rows):
-            ti = getattr(self.tbrowser, 'table_item_{}'.format(i))
-            if ti.checkState() == Qt.Checked:
-                checked_rows.append((i, u))
-        for i, u in checked_rows:
-            orderthread = OrderThread(row_index=i, user_row=u, tbrowser=self.tbrowser, ordths=orderthreads)
-            orderthreads.append(orderthread)
+        print(lineno(), len(user_rows))
+
+        checked_rows = self.get_checked_rows(u_rows=user_rows)
+        print(lineno(), checked_rows)
+        thread_num = self.get_thread_num()
+        for i in checked_rows:
+            orderthread = OrderThread(
+                row_index=i[0], user_row=i[1],
+                tbrowser=self.tbrowser,)
             setattr(orderthread, 'exiting', False)
-            orderthread.start()
-            print('%s: Is [Order Thread %s] Alive? %s' %
-                  (lineno(), orderthread.name, orderthread.isRunning()))
+            pool_inst = QThreadPool.globalInstance()
+            pool_inst.setMaxThreadCount(int(thread_num))
+            pool_inst.start(orderthread)
+            print('%s: Is [Order Thread %s] Alive?' %
+                  (lineno(), orderthread.name))
 
     def prestoporder(self):
+        """
+        """
         self.orderthread.sg.selected.connect(self.stoporder)
 
     def stoporder(self):
+        """
+        """
         self.orderthread.quit()
 
 
 class FileWidget(QWidget):
+    """
+    """
+
     def __init__(self):
         super(FileWidget, self).__init__()
         gridlayout = QGridLayout()
@@ -313,36 +394,64 @@ class FileWidget(QWidget):
 
 
 class OrderWidget(QWidget):
+    """
+    """
+
     def __init__(self):
         super(OrderWidget, self).__init__()
 
 
 class SetPanel(QWidget):
-    def __init__(self):
-        super(SetPanel, self).__init__()
+    """
+    This Class used to save user specific settings.
+    """
 
+    def __init__(self):
+        """
+        """
+        super(SetPanel, self).__init__()
         self.threadCountLabel = QLabel(u"使用线程数:")
         self.threadCountEdit = QLineEdit()
+        try:
+            assert 'user_config.json' in current_f_list
+            with open('user_config.json') as raw_f:
+                f = json.load(raw_f)
+            user_thread_num = f.get('user_thread_num')
+            self.threadCountEdit.setText(unicode(user_thread_num))
+        except Exception as e:
+            self.threadCountEdit.setText(unicode(default_thread_num))
         self.savebutton = QPushButton(u'保存')
+        self.unlockbutton = QPushButton(u'解锁')
+        self.resetbutton = QPushButton(u'恢复初始设置参数')
         self.threadCountLabel.setFixedSize(90, 35)
         formlayout = QFormLayout()
         formlayout.setSpacing(10)
         formlayout.addWidget(self.threadCountLabel)
         formlayout.addWidget(self.threadCountEdit, )
+        formlayout.addWidget(self.unlockbutton)
         formlayout.addWidget(self.savebutton)
         self.savebutton.clicked.connect(self.pass_count)
-
+        self.unlockbutton.clicked.connect(self.unlock_edit)
+        self.threadCountEdit.setEnabled(False)
+        self.setToolTip(u'点击解锁后启动编辑功能')
         self.setLayout(formlayout)
 
     def pass_count(self):
         try:
-            thread_Num = self.threadCountEdit.text()
+            user_thread_num = self.threadCountEdit.text()
+            with codecs.open('user_config.json', 'w', 'utf8') as f:
+                f.write(json.dumps({'user_thread_num': user_thread_num},
+                                   sort_keys=True, ensure_ascii=False))
+            self.threadCountEdit.setEnabled(False)
         except Exception as e:
             raise e
         else:
             pass
         finally:
             pass
+
+    def unlock_edit(self):
+        self.threadCountEdit.setEnabled(True)
 
 
 if __name__ == '__main__':
